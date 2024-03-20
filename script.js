0e8839469e6860eb3e7e3e0f2527731f900c48c9
ezhang04 <97803134+ezhang04@users.noreply.github.com>
2024-03-20 17:59:57 -0400

    Added rocket animation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const textElement = document.getElementById('word_change');
 const texts = ["<strong>Susan Zhang</strong>", "<strong>An Electrical Engineer</strong>", "<strong>An Avid Scholar</strong>", "<strong >A Systems Engineer</strong >"];
 
@@ -73,16 +72,3 @@ function createSmoke() {
         });
     }
 }
-=======
-const textElement = document.getElementById('word_change');
-const texts = ["<strong>Susan Zhang</strong>", "<strong>An Electrical Engineer</strong>", "<strong>An Avid Scholar</strong>", "<strong >A Systems Engineer</strong >"];
-
-let index = 0;
-
-function changeText() {
-    textElement.innerHTML = texts[index];
-    index = (index + 1) % texts.length;
-}
-changeText();
-setInterval(changeText, 1500);
->>>>>>> e2ce1a938b44662b9476913e8a9180f328ed40f6
