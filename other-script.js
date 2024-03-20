260556861fcc4951f74004d48f95fd2f773482b2
ezhang04 <97803134+ezhang04@users.noreply.github.com>
2024-03-20 17:49:15 -0400

    Added Rocket animation when redirect

--- other-script.js
+++ other-script.js
@@ -1,15 +1,3 @@
-const textElement = document.getElementById('word_change');
-const texts = ["<strong>Susan Zhang</strong>", "<strong>An Electrical Engineer</strong>", "<strong>An Avid Scholar</strong>", "<strong >A Systems Engineer</strong >"];
-
-let index = 0;
-
-function changeText() {
-    textElement.innerHTML = texts[index];
-    index = (index + 1) % texts.length;
-}
-changeText();
-setInterval(changeText, 1500);
-
 // Dynamically load Anime.js library
 var script = document.createElement('script');
 script.src = 'https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js';
